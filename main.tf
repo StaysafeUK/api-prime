a7863d3ebc27adf8a92400ab86f9858f59200c87
JustinEvans <justin.robert.evans@protonmail.com>
2025-07-02 09:36:06 +0100

    add skeleton tf main.tf JRE 02072025 0936

---
+++ main.tf
@@ -0,0 +1,38 @@
+provider "google" {
+  credentials = file("credentials.json")
+  project     = "archejre"
+  region      = "europe-west2"
+  zone        = "europe-west2-a"
+}
+
+terraform {
+  required_version = ">= 1.0.11"
+  required_providers {
+    google = {
+
+
+      source  = "hashicorp/google"
+      version = ">= 4.5.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "2.1.0"
+    }
+  }
+}
+
+variable "vm_names" {
+  type    = list(string)
+  default = ["front-end", "back-end", "database"]
+}
+
+module "vm" {
+  source  = "./modules/vm"
+  vm-name = var.vm_names[count.index]
+  count   = length(var.vm_names)
+}
+
+resource "local_file" "IPs" {
+  filename = "./inventory.csv"
+  content  = templatefile("manifest.tftpl", { ip_addrs = module.vm.*.ip })
+}
