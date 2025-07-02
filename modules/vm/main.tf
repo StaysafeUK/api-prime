1a5d5664878c806af0e80f2bc8ca1f8c4bb24710
JustinEvans <justin.robert.evans@protonmail.com>
2025-07-02 10:08:41 +0100

    add startup script for docker,nginx and django rest framework 020072025 1008 JRE

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -20,6 +20,46 @@ resource "google_compute_instance" "vm" {
       // Ephemeral external IP
     }
   }
+
+  metadata_startup_script = <<-EOF
+    #!/bin/bash
+    # Update and install packages
+    apt-get update
+    apt-get install -y python3-pip nginx
+
+    # Install Docker
+    apt-get install -y docker.io
+    systemctl start docker
+    systemctl enable docker
+
+    # Install Django and Django REST Framework
+    pip3 install django djangorestframework
+
+    # Create Django project
+    django-admin startproject api_project /srv/api_project
+    cd /srv/api_project
+    python3 manage.py startapp api
+
+    # Configure Nginx
+    cat <<'EOT' > /etc/nginx/sites-available/default
+    server {
+        listen 80;
+        server_name _;
+
+        location / {
+            proxy_pass http://127.0.0.1:8000;
+            proxy_set_header Host $host;
+            proxy_set_header X-Real-IP $remote_addr;
+            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+            proxy_set_header X-Forwarded-Proto $scheme;
+        }
+    }
+EOT
+    systemctl restart nginx
+
+    # Start Django app
+    cd /srv/api_project && python3 manage.py runserver 0.0.0.0:8000 &
+    EOF
 }
 
 output "ip" {
