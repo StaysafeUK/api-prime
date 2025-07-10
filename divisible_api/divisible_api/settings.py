e06f389a74ef077dba95d04d041c87835474925f
JustinEvans <justin.robert.evans@protonmail.com>
2025-07-10 11:45:58 +0100

    add settings.py SECRET_KEY and set debug to False final JRE 10072025 1145

--- divisible_api/divisible_api/settings.py
+++ divisible_api/divisible_api/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)tq@s6)%6ut)ugx90o$2nez@$qv@(anbmhs2u5bc2(mj=1%7ae'
+SECRET_KEY = '_+m*mm9@*t=wjd2%nbn_iat6_e#pnd&3+d)cc#(1tt@c01w%i%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
