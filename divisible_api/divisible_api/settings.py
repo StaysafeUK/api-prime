073842482b1e755268577ffab785910a6357995c
JustinEvans <justin.robert.evans@protonmail.com>
2025-07-28 07:01:42 +0100

    Trigger workflow again

--- divisible_api/divisible_api/settings.py
+++ divisible_api/divisible_api/settings.py
@@ -12,8 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
-# Trigger workflow
-
+# Trigger workflow again
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
