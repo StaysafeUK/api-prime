4aeee9c1bac18d14e0b9d94cd29974a85e67780e
JustinEvans <justin.robert.evans@protonmail.com>
2025-08-29 22:01:41 +0100

    P12 add Celery worker requirements.txt missing dependencies  JRE 29052025 2201

--- modules/worker/main.tf
+++ modules/worker/main.tf
@@ -84,7 +84,7 @@ rm -rf /srv/prime-number-api/venv
 python3 -m venv /srv/prime-number-api/venv
 
 # Install Python dependencies for the worker
-/srv/prime-number-api/venv/bin/pip install --no-cache-dir -r /srv/prime-number-api/modules/worker/requirements.txt
+/srv/prime-number-api/venv/bin/pip install --no-cache-dir -r /srv/prime-number-api/requirements.txt
 
 # Get the Redis host IP address from metadata and set it as an environment variable
 export REDIS_HOST=$(curl -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/attributes/redis-host)
