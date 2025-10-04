c6844196409df4ed3789ffd45b8eda702d422739
JustinEvans <justin.robert.evans@protonmail.com>
2025-10-04 19:14:11 +0100

    P4 add correct permissions to correct service account JRE 04102025 1914

--- service_account.tf
+++ service_account.tf
@@ -1,31 +1,27 @@
-resource "google_service_account" "api_prime_svc_net" {
-  account_id   = "api-prime-svc-net"
-  display_name = "API Prime Service Account"
-  project      = "archejrenet-dev-03102025"
-}
+
 
 resource "google_project_iam_member" "api_prime_svc_net_compute_instance_admin" {
   project = "archejrenet-dev-03102025"
   role    = "roles/compute.instanceAdmin.v1"
-  member  = "serviceAccount:${google_service_account.api_prime_svc_net.email}"
+  member  = "serviceAccount:terra-svc-net@archejrenet.iam.gserviceaccount.com"
 }
 
 resource "google_project_iam_member" "api_prime_svc_net_compute_network_admin" {
   project = "archejrenet-dev-03102025"
   role    = "roles/compute.networkAdmin"
-  member  = "serviceAccount:${google_service_account.api_prime_svc_net.email}"
+  member  = "serviceAccount:terra-svc-net@archejrenet.iam.gserviceaccount.com"
 }
 
 resource "google_project_iam_member" "api_prime_svc_net_compute_security_admin" {
   project = "archejrenet-dev-03102025"
   role    = "roles/compute.securityAdmin"
-  member  = "serviceAccount:${google_service_account.api_prime_svc_net.email}"
+  member  = "serviceAccount:terra-svc-net@archejrenet.iam.gserviceaccount.com"
 }
 
 resource "google_project_iam_member" "api_prime_svc_net_secretmanager_admin" {
   project = "archejrenet-dev-03102025"
   role    = "roles/secretmanager.admin"
-  member  = "serviceAccount:${google_service_account.api_prime_svc_net.email}"
+  member  = "serviceAccount:terra-svc-net@archejrenet.iam.gserviceaccount.com"
 }
 
 resource "google_project_service" "secretmanager_api" {
